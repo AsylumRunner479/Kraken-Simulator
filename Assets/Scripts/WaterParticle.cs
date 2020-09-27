a8e5148a9127947352e40e583e1ed003a202ab80
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-09-27 14:57:28 +1000

    added water particles

---
+++ Assets/Scripts/WaterParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterParticle : MonoBehaviour
+{
+    public Transform originalPos;
+    public float normSpeed;
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 end = originalPos.position;
+        self.transform.position = Vector3.Lerp(self.transform.position, end, normSpeed);
+    }
+}
